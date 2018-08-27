170c5bb9b1edf3ecb01773e2da0d74df97de1ec2
maltebacher <40758081+maltebacher@users.noreply.github.com>
2018-08-27 17:47:37 +0200

    Add files via upload

--- Configuration/TypoScript/constants.ts
+++ Configuration/TypoScript/constants.ts
@@ -1,1 +1,14 @@
 
+plugin.tx_speakerlist_speakerlistfe {
+    view {
+        # cat=plugin.tx_speakerlist_speakerlistfe/file; type=string; label=Path to template root (FE)
+        templateRootPath = EXT:speakerlist/Resources/Private/Templates/
+        # cat=plugin.tx_speakerlist_speakerlistfe/file; type=string; label=Path to template partials (FE)
+        partialRootPath = EXT:speakerlist/Resources/Private/Partials/
+        # cat=plugin.tx_speakerlist_speakerlistfe/file; type=string; label=Path to template layouts (FE)
+        layoutRootPath = EXT:speakerlist/Resources/Private/Layouts/
+    }
+    persistence {
+        # cat=plugin.tx_speakerlist_speakerlistfe//a; type=string; label=Default storage PID
+    }
+}

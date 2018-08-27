d87dfbeab2f78c49e4c3b78bb3735d9fbd35e6ef
maltebacher <40758081+maltebacher@users.noreply.github.com>
2018-08-27 17:54:19 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.2.0-9.5.99',
+            'typo3' => '8.7.19-9.5.99',
             'sf_event_mgt' => '3.0.5-',
         ],
         'conflicts' => [],
